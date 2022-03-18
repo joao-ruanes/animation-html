de1f1abf40792f6ab0af2d738c97fe5f44aa365c
joao-ruanes <joaovitor-ribeiro@hotmail.com>
2022-03-18 13:04:28 -0300

    Add index

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+let player = document.getElementById("player");
+
+let xInitial = 0;
+let yInitial = 0;
+
+function movePlayerTo(x, y) {
+
+    let posX = x + "px";
+    let posY = y + "px";
+
+
+    player.style.top = posX;
+    player.style.left = posY;
+}
+
+
+setInterval(function () {
+
+    movePlayerTo(xInitial++, yInitial++)
+
+}, 24)
